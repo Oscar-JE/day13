0bac276a7ee1b1e36a74eb717c47486b712621a6
Oscar Johansson Elling <oscar.johansson.elling@gmail.com>
2023-09-28 23:31:11 +0200

    inputRep

--- compare/compare.go
+++ compare/compare.go
@@ -1,5 +1,62 @@
 package compare
 
-func FirstLargerThanSecond(first string, second string) bool {
-	return true
+import (
+	"strconv"
+)
+
+type InputRep struct {
+	isLeft  bool
+	isRight bool
+	value   int
+}
+
+func initLeft() InputRep {
+	return InputRep{true, false, -1}
+}
+
+func initRight() InputRep {
+	return InputRep{false, true, -1}
+}
+
+func initNumber(number int) InputRep {
+	return InputRep{false, false, number}
+}
+
+func pairsInOrder(first string, second string) bool {
+	return false
+}
+
+func equal(a InputRep, b InputRep) bool {
+	return a.isLeft == b.isLeft && a.isRight == b.isRight && a.value == b.value
+}
+
+func parseToInputRep(line string) []InputRep {
+	retValue := []InputRep{}
+	numberBuffer := ""
+	for index := range line {
+		if line[index] == '[' {
+			retValue = append(retValue, initLeft())
+		} else if line[index] == ']' {
+			if numberBuffer != "" {
+				value, errorConv := strconv.Atoi(numberBuffer)
+				if errorConv != nil {
+					panic("parsing error")
+				}
+				retValue = append(retValue, initNumber(value))
+			}
+			retValue = append(retValue, initRight())
+		} else if line[index] == ',' {
+			if numberBuffer != "" {
+				value, errorConv := strconv.Atoi(numberBuffer)
+				if errorConv != nil {
+					panic("parsing error")
+				}
+				retValue = append(retValue, initNumber(value))
+				numberBuffer = ""
+			}
+		} else {
+			numberBuffer += string(line[index])
+		}
+	}
+	return retValue
 }
--- compare/compare_test.go
+++ compare/compare_test.go
@@ -1,1 +1,103 @@
 package compare
+
+import (
+	"testing"
+)
+
+func TestCompare1(t *testing.T) {
+	a := "[1,1,3,1,1]"
+	b := "[1,1,5,1,1]"
+	if !pairsInOrder(a, b) {
+		t.Errorf("error in case 1")
+	}
+}
+
+func TestCompare2(t *testing.T) {
+	a := "[[1],[2,3,4]]"
+	b := "[[1],4]"
+	if !pairsInOrder(a, b) {
+		t.Errorf("error in case 2 ")
+	}
+}
+
+func TestCompare3(t *testing.T) {
+	a := "[9]"
+	b := "[[8,7,6]]"
+	if pairsInOrder(a, b) {
+		t.Errorf("error in case 3 ")
+	}
+}
+
+func TestCompare4(t *testing.T) {
+	a := "[[4,4],4,4]"
+	b := "[[4,4],4,4,4]"
+	if !pairsInOrder(a, b) {
+		t.Errorf("error in case 4 ")
+	}
+}
+
+func TestCompare5(t *testing.T) {
+	a := "[7,7,7,7]"
+	b := "[7,7,7]"
+	if pairsInOrder(a, b) {
+		t.Errorf("error in case 5")
+	}
+}
+
+func TestCompare6(t *testing.T) {
+	a := "[]"
+	b := "[3]"
+	if !pairsInOrder(a, b) {
+		t.Errorf("error in case 6")
+	}
+}
+
+func TestCompare7(t *testing.T) {
+	a := "[[[]]]"
+	b := "[[]]"
+	if pairsInOrder(a, b) {
+		t.Errorf("error in case 7")
+	}
+}
+
+func TestCompare8(t *testing.T) {
+	a := "[1,[2,[3,[4,[5,6,7]]]],8,9]"
+	b := "[1,[2,[3,[4,[5,6,0]]]],8,9]"
+	if pairsInOrder(a, b) {
+		t.Errorf("error in case 8")
+	}
+}
+
+func TestParseToInputRep(t *testing.T) {
+	a := "[1,12]"
+	res := parseToInputRep(a)
+	expected := []InputRep{initLeft(), initNumber(1), initNumber(12), initRight()}
+	if !sliceEqual(res, expected) {
+		t.Errorf("error in simple conversion to InputRep")
+	}
+}
+
+func TestComplexParseToInputRep(t *testing.T) {
+	a := "[1,[2,[],23]"
+	expected := []InputRep{initLeft(), initNumber(1), initLeft(), initNumber(2), initLeft(), initRight(), initNumber(23), initRight()}
+	res := parseToInputRep(a)
+	if !sliceEqual(expected, res) {
+		t.Errorf("error in complex conversion to InputRep")
+	}
+}
+
+func sliceEqual(vec1 []InputRep, vec2 []InputRep) bool {
+	isEqual := true
+	maxLen := max(len(vec1), len(vec2))
+	for i := 0; i < maxLen; i++ {
+		isEqual = isEqual && equal(vec1[i], vec2[i])
+	}
+	return isEqual
+}
+
+func max(a int, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
